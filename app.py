b4a05e351fba65b1e131641689749b55f12359e8
Varun Suraj <varunsuraj@college.harvard.edu>
2022-12-05 12:30:23 -0500

    removing unnecessary imports

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import os
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
